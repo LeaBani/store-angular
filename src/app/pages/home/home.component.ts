ec826cf8580db20ada067c00bc8cc170150ebeee
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-05-03 15:58:45 +0200

    add categories

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,13 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  cols =3 ; // on intialise le nombre de colonnes a afficher
+  cols =3 ; // on intialise le nombre de colonnes a afficher (par défaut)
   constructor(){}
 
   ngOnInit(): void {
     
   }
 
+  /**
+   * ici on modifie le nombre de colonnes a afficher 
+   * en cliquant sur les icones, on passe à la variable une nouvelle valeur
+   * @param colsNum 
+   */
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
   }
