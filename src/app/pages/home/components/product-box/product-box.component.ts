7a460beb81110f525fb38be34b6b4aaa6bd58a1c
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-05-05 00:26:12 +0200

    add cards with dynamic fullWidthMode

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
   templateUrl: 'product-box.component.html'
 })
-export class ProductBoxComponent {
+export class ProductBoxComponent implements OnInit {
+  @Input() fullWidthMode = false; // sur la vue avec 1 seule colonne
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
